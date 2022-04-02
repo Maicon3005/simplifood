eb952e54e98235bad177aa1bd4a8111285b1e9f7
Maicon3005 <maiconlotel@gmail.com>
2022-04-02 16:14:19 -0300

    Tela de pedido em andamento

--- app-client/src/ui/screens/carte/carte.jsx
+++ app-client/src/ui/screens/carte/carte.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import { Base } from "../../components/";
 import "./style.css";
 
 export function Carte() {
+  const [categorys, setCategorys] = useState([]);
+
+  useEffect(() => {
+    setCategorys([
+      { id: "1", category: "Lanches" },
+      { id: "2", category: "Massas" },
+      { id: "3", category: "Escondidinhos" },
+      { id: "4", category: "Bebidas" },
+    ]);
+  }, []);
   return (
     <Base>
-      <p>Hello word</p>
+      <div className="container-carte">
+        <ul className="container-categories">
+          {categorys.map((category) => (
+            <li className="container-category" key={category.id}>
+              <h3>{category.category}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Base>
   );
 }
